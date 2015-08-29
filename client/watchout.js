699cb3a3d266440f4fd39c2350b0ecf96622027a
jolthoff <jl.olthoff@gmail.com>
2015-08-28 21:53:55 -0700

    Implement collision detections and scoring

--- client/watchout.js
+++ client/watchout.js
@@ -4,7 +4,7 @@ var Game = function(){
     'playerState': {},
     'enemyStates': []
   };
-  this.asteroidCount = 30;
+  this.asteroidCount = 15;
   this.displayCanvas = d3.select("body").append("svg")
     .attr('width', 1000)
     .attr('height', 1000);
@@ -17,6 +17,9 @@ var Game = function(){
   this.player = this.displayCanvas.append("circle")
     .attr('fill', 'yellow')
     .attr('class', 'player');
+  this.collisionCount = 0;
+  this.curScore = 0;
+  this.highScore = 0;
   this.runGame();
 }
 
@@ -28,6 +31,11 @@ Game.prototype.runGame = function() {
     this.updateBoard();
     this.renderBoard();
   }.bind(this), 1000);
+
+  setInterval(function() {
+    this.score();
+    this.collisionDetection();
+  }.bind(this), 100);
 }
 
 Game.prototype.createPlayer = function() {
@@ -93,15 +101,55 @@ Game.prototype.renderBoard = function() {
     .attr('xlink:href', 'asteroid.png');
 
   joinedAsteroids.transition()
-    .duration(750)
+    .duration(1000)
     .attr('x', function(d, i) {return d.x})
     .attr('y', function(d, i) {return d.y});
-    
+  
+  d3.select('.collisions').select('span').text(this.collisionCount);
   joinedAsteroids.exit().remove();
 }
 
 Game.prototype.createEnemies = function() {
   this.createAsteroids();
+}
+
+Game.prototype.checkCollision = function (dThreeObject) {
+  var dThreePlayer = d3.select('.player');
+
+  var asteroidX = dThreeObject.attr('x');
+  var asteroidY = dThreeObject.attr('y');
+  var asteroidR = dThreeObject.attr('width');
+  var playerX = dThreePlayer.attr('cx');
+  var playerY = dThreePlayer.attr('cy');
+  var playerR = dThreePlayer.attr('r');
+
+  var diffX = playerX - asteroidX;
+  var diffY = playerY - asteroidY;
+  var radiusSum = parseFloat(playerR) + parseFloat(asteroidR) / 2;
+  var separation = Math.sqrt(diffX*diffX + diffY*diffY);
+
+  if(separation < radiusSum) {
+    this.collisionCount++;
+    this.highScore = Math.max(this.curScore, this.highScore);
+    this.curScore = 0;
+    return true;
+  }
+  else
+    return false;
+}
+
+Game.prototype.score = function () {
+  this.curScore += 1;
+  d3.select('.high').select('span').text(this.highScore);
+  d3.select('.current').select('span').text(this.curScore);
+  
+}
+
+Game.prototype.collisionDetection = function() {
+  var joinedAsteroids = this.asteroids.selectAll('.asteroid')[0];
+  for (var i = 0; i < joinedAsteroids.length; i++) {
+    this.checkCollision(d3.select(joinedAsteroids[i]));
+  }
 }
 
 Game.prototype.createAsteroids = function() {
